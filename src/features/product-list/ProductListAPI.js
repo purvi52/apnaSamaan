966b5555c88676939954b94f73785d673a774a07
purvi52 <purvipurwar52@gmail.com>
2023-12-20 11:46:35 +0530

    did filtering and component division

--- src/features/product-list/ProductListAPI.js
+++ src/features/product-list/ProductListAPI.js
@@ -1,7 +1,33 @@
-export function fetchCount(amount = 1) {
+export function fetchAllProducts() {
     return new Promise(async (resolve) =>
     {
-      const response=await fetch('http://localhost:8080')
+      const response=await fetch('http://localhost:8080/products')
+      const data=await response.json()
+      resolve({data})
+    }
+    );
+  }
+
+  export function fetchProductsByFilters(filter,sort) {
+    let queryString='';
+    for(let key in filter)
+    {
+      const categoryValues=filter[key];
+      if(categoryValues.length>0)
+      {
+        const lastCategoryValue=categoryValues[categoryValues.length-1]
+        queryString+=`${key}=${lastCategoryValue}&`
+      }  
+    }
+    
+    for(let key in sort)
+    {
+      queryString+=`${key}=${sort[key]}&`
+    }
+
+    return new Promise(async (resolve) =>
+    {
+      const response=await fetch('http://localhost:8080/products?'+queryString)
       const data=await response.json()
       resolve({data})
     }
